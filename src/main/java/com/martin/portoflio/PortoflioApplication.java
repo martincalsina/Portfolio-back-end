c894893d5b832a34775cc943af27a7f56677ba41
martin.calsina <martincalsina05@gmail.com>
2024-01-16 15:39:14 -0300

    creacion del proyecto spring

---
+++ src/main/java/com/martin/portoflio/PortoflioApplication.java
@@ -0,0 +1,13 @@
+package com.martin.portoflio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortoflioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortoflioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martin/portoflio/PortoflioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.portoflio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortoflioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
